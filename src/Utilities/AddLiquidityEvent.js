ee8e0a417448bbf20c3e6e635200d146e8afbe54
Whysaputro <hendrawahyu26@gmail.com>
2022-03-11 12:07:07 +0700

    add more AddLiquidityEvent

--- src/Utilities/AddLiquidityEvent.js
+++ src/Utilities/AddLiquidityEvent.js
@@ -24,4 +24,8 @@ const addLiquidity = {
   ],
 };
 
-module.exports = { addLiquidityETH, addLiquidity };
+const finalize = {
+  hex: /^0x4bb278f3/,
+};
+
+module.exports = { addLiquidityETH, addLiquidity, finalize };
