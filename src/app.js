7c0def5d67aa68caeb48d046d3df7673152ff59d
Whysaputro <hendrawahyu26@gmail.com>
2022-03-23 20:14:39 +0700

    feat: check tax fee on token and refactor the task

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const Web3 = require('web3');
 const colors = require('colors/safe');
 const axios = require('axios');
 const { Listr } = require('listr2');
-const { banner } = require('./libs/banner');
+const { banner } = require('./Libraries/banner');
 const { addLiquidityETH, addLiquidity } = require('./Utilities/liquidityEvents');
 const { finalize } = require('./Utilities/presaleEvents');
 const { BEP20ABI, PancakeRouterABI } = require('./Utilities/ABI');
+const { Token } = require('./Class/Token');
 
 const websocketOptions = {
   timeout: 30000, // ms
@@ -33,7 +34,6 @@ const websocketOptions = {
 const websocket = new Web3.providers.WebsocketProvider(process.env.WEBSOCKET, websocketOptions);
 const web3 = new Web3(websocket);
 const pancakeswap = new web3.eth.Contract(PancakeRouterABI, process.env.PANCAKE_ROUTER);
-const mempool = web3.eth.subscribe('pendingTransactions');
 
 web3.eth.Contract.handleRevert = true;
 web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
@@ -63,6 +63,13 @@ function buyToken(fromToken, toToken, purchaseAmount) {
     pancakeswap.methods.getAmountsOut(amountsIn, [fromToken, toToken]).call({ from: process.env.WALLET_ADDRESS })
       .then((amountsOut) => {
         const amountsOutMin = web3.utils.toBN(amountsOut[1] * (1 - process.env.SLIPPAGE / 100));
+
+        checkTokenTax(toToken).then((tax) => {
+          if (tax.buyTax > 20 ) {
+            reject(new Error('Buy tax more than expected'));
+          }
+        })
+
         pancakeswap.methods.swapExactTokensForTokens(
           amountsIn,
           amountsOutMin,
@@ -90,6 +97,13 @@ function buyTokenWithBNB(toToken, purchaseAmount) {
     pancakeswap.methods.getAmountsOut(amountsIn, [process.env.WBNB_ADDRESS, toToken]).call({ from: process.env.WALLET_ADDRESS })
       .then((amountsOut) => {
         const amountsOutMin = web3.utils.toBN(amountsOut[1] * (1 - process.env.SLIPPAGE / 100));
+
+        checkTokenTax(toToken).then((tax) => {
+          if (tax.buyTax > 20 ) {
+            reject(new Error('Buy tax more than expected'));
+          }
+        })
+
         pancakeswap.methods.swapExactETHForTokens(
           amountsOutMin,
           [process.env.WBNB_ADDRESS, toToken],
@@ -112,11 +126,10 @@ function buyTokenWithBNB(toToken, purchaseAmount) {
   });
 }
 
-function honeyPotCheck(tokenAddress) {
+function checkTokenTax(tokenAddress) {
   return new Promise((resolve, reject) => {
-    axios.get('https://api.banditcoding.xyz/honeypot/', {
+    axios.get('https://api.cyberoni.org/honeypot/', {
       params: {
-        chain: 'bsc',
         address: tokenAddress,
       },
     })
@@ -129,7 +142,7 @@ function honeyPotCheck(tokenAddress) {
   });
 }
 
-function scanningMempool(tokenAddress) {
+function scanningMempool(tokenAddress, mempool) {
   return new Promise((resolve, reject) => {
     mempool.on('data', (txHash) => {
       web3.eth.getTransaction(txHash, (error, transaction) => {
@@ -164,7 +177,7 @@ function scanningMempool(tokenAddress) {
   });
 }
 
-function scanMempoolForPresale(presaleAddress) {
+function scanMempoolForPresale(presaleAddress, mempool) {
   return new Promise((resolve, reject) => {
     mempool.on('data', (txHash) => {
       web3.eth.getTransaction(txHash, (error, transaction) => {
@@ -179,86 +192,155 @@ function scanMempoolForPresale(presaleAddress) {
 }
 
 async function init() {
-  banner.show();
+  try {
+    banner.show();
 
-  const tasks = new Listr([
-    {
-      title: colors.blue('Initiating Sniper BOT...!'),
-      task: () => new Listr([
-        {
-          title: 'Token address ?',
-          task: async (ctx, task) => {
-            ctx.tokenAddress = await task.prompt({
-              type: 'input',
-              message: 'Input token address want to snipe',
-              required: true,
-            });
-          },
-        },
-        {
-          title: 'Token pair ?',
-          task: async (ctx, task) => {
-            ctx.tokenPair = await task.prompt({
-              type: 'select',
-              message: 'Choose token pair',
-              choices: ['BNB', 'BUSD', 'USDT'],
-            });
-          },
-        },
-        {
-          title: 'How much BNB/BUSD/USDT will you use to buy the token ?',
-          task: async (ctx, task) => {
-            ctx.purchaseAmount = await task.prompt({
-              type: 'numeral',
-              message: 'Input WBNB/BUSD/USDT amount',
-              initial: 0.1,
-            });
-          },
-        },
-      ], { concurrent: false }),
-    },
-    {
-      title: 'Approving BUSD/USDT token',
-      skip: (ctx) => ctx.tokenPair === 'BNB',
-      task: async (ctx, task) => {
-        const tokenPairAddress = ctx.tokenPair === 'BUSD' ? process.env.BUSD_ADDRESS : process.env.USDT_ADDRESS;
-        const txHash = await approveToken(tokenPairAddress, ctx.purchaseAmount);
-        task.title = `${colors.green(`${ctx.tokenPair} approved for trade:`)} https://bscscan.com/tx/${txHash}`;
-      },
-    },
-    {
-      title: 'Scanning mempool...',
-      task: async (ctx, task) => {
-        const txHash = await scanningMempool(ctx.tokenAddress);
-        task.title = `${colors.green('Liquidity transaction detected in mempool:')} https://bscscan.com/tx/${txHash}`;
-        mempool.unsubscribe();
-      },
-    },
-    {
-      title: 'Snipping the token',
-      task: async (ctx, task) => {
-        if (ctx.tokenPair === 'BNB') {
-          const txHash = await buyTokenWithBNB(ctx.tokenAddress, ctx.purchaseAmount);
-          task.title = `${colors.green('Sniping success:')} https://bscscan.com/tx/${txHash}`;
-        }
+    const ctx = {};
 
-        if (ctx.tokenPair !== 'BNB') {
+    const initializeTask = new Listr([
+      {
+        title: colors.blue('Initiating Sniper BOT...!'),
+        task: (_, task) => task.newListr([
+          {
+            title: 'Token address ?',
+            task: async (ctx, task) => {
+              ctx.tokenAddress = await task.prompt({
+                type: 'input',
+                message: 'Input token address want to snipe',
+                required: true,
+              });
+
+              task.title = `Token Detail:`;
+
+              ctx.tokenInstance = new Token(ctx.tokenAddress, BEP20ABI, web3);
+
+              return task.newListr([
+                {
+                  title: 'Checking token address',
+                  task: async (ctx, task) => {
+                    task.title = `Token address: ${ await ctx.tokenAddress }`;
+                  }
+                },
+                {
+                  title: 'Checking token name',
+                  task: async (ctx, task) => {
+                    task.title = `Token name: ${ await ctx.tokenInstance.name() }`;
+                  }
+                },
+                {
+                  title: 'Checking token symbol',
+                  task: async (ctx, task) => {
+                    task.title = `Token symbol: ${ await ctx.tokenInstance.symbol() }`;
+                  }
+                },
+                {
+                  title: 'Checking token decimals',
+                  task: async (ctx, task) => {
+                    task.title = `Token decimals: ${ await ctx.tokenInstance.decimals() }`;
+                  }
+                },
+              ], { concurrent: true })
+            },
+          },
+          {
+            title: 'Token pair ?',
+            task: async (ctx, task) => {
+              ctx.tokenPair = await task.prompt({
+                type: 'select',
+                message: 'Choose token pair',
+                choices: ['BNB', 'BUSD', 'USDT'],
+              });
+
+              task.title = `Token pair: ${ctx.tokenPair}`;
+            },
+          },
+          {
+            title: 'How much BNB/BUSD/USDT will you use to buy the token ?',
+            task: async (ctx, task) => {
+              ctx.purchaseAmount = await task.prompt({
+                type: 'numeral',
+                message: 'Input WBNB/BUSD/USDT amount',
+                initial: 0.1,
+              });
+
+              task.title = `How much will you buy: ${ctx.purchaseAmount} ${ctx.tokenPair}`;
+            },
+          },
+          {
+            title: 'Does the token launch IDO on PinkSale ?',
+            task: async (ctx, task) => {
+              ctx.pinkSaleIDO = await task.prompt({
+                type: 'Toggle',
+                message: 'Choose the options',
+                initial: true,
+              });
+
+              task.title = `Does the token launch IDO on PinkSale: ${ctx.pinkSaleIDO ? 'Yes' : 'No'}`;
+            },
+          },
+        ], { ctx, concurrent: false, rendererOptions: { collapse: false } }),
+      }
+    ])
+
+    const snipeTokenTask = new Listr([
+      {
+        title: 'Approving BUSD/USDT token',
+        skip: (ctx) => ctx.tokenPair === 'BNB',
+        task: async (ctx, task) => {
           const tokenPairAddress = ctx.tokenPair === 'BUSD' ? process.env.BUSD_ADDRESS : process.env.USDT_ADDRESS;
-          const txHash = await buyToken(tokenPairAddress, ctx.tokenAddress, ctx.purchaseAmount);
-          task.title = `${colors.green('Sniping success:')} https://bscscan.com/tx/${txHash}`;
-        }
+          const txHash = await approveToken(tokenPairAddress, ctx.purchaseAmount);
+          task.title = `${colors.green(`${ctx.tokenPair} approved for trade:`)} https://bscscan.com/tx/${txHash}`;
+        },
       },
-      retry: 5,
-    },
-  ], {
-    concurrent: false,
-    exitOnError: true,
-    registerSignalListeners: false,
-  });
+      {
+        title: 'Scanning mempool...',
+        task: async (ctx, task) => {
+          const mempool = web3.eth.subscribe('pendingTransactions');
 
-  tasks.run().then(() => {
-    process.exit(0);
-  });
+          if (!ctx.pinkSaleIDO) {
+            const txHash = await scanningMempool(ctx.tokenAddress, mempool);
+            task.title = `${colors.green('Liquidity transaction detected in mempool:')} https://bscscan.com/tx/${txHash}`;
+          } else {
+            const txHash = await scanMempoolForPresale(ctx.tokenAddress, mempool);
+            task.title = `${colors.green('Liquidity transaction detected in mempool:')} https://bscscan.com/tx/${txHash}`;
+          }
+
+          mempool.unsubscribe();
+        },
+      },
+      {
+        title: 'Snipping the token',
+        task: async (ctx, task) => {
+          if (ctx.tokenPair === 'BNB') {
+            const txHash = await buyTokenWithBNB(ctx.tokenAddress, ctx.purchaseAmount);
+            task.title = `${colors.green('Sniping success:')} https://bscscan.com/tx/${txHash}`;
+          }
+
+          if (ctx.tokenPair !== 'BNB') {
+            const tokenPairAddress = ctx.tokenPair === 'BUSD' ? process.env.BUSD_ADDRESS : process.env.USDT_ADDRESS;
+            const txHash = await buyToken(tokenPairAddress, ctx.tokenAddress, ctx.purchaseAmount);
+            task.title = `${colors.green('Sniping success:')} https://bscscan.com/tx/${txHash}`;
+          }
+        },
+        retry: 100,
+      },
+    ], {
+      ctx,
+      concurrent: false,
+      exitOnError: true,
+      registerSignalListeners: false,
+    });
+
+    await initializeTask.run({ ctx });
+    
+    await snipeTokenTask.run({ ctx })
+    .then(() => {
+      process.exit(0);
+    })
+
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 init();
